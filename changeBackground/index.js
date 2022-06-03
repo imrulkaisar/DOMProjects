493a718cad81f484f9046323c3f8cd9d2bdec5c2
Imrul Kaisar <imrulkaisar3@gmail.com>
2022-06-03 18:22:48 +0600

    Found issue on result showing feature

--- changeBackground/index.js
+++ changeBackground/index.js
@@ -5,7 +5,9 @@
  * 1) Create Oneload handeler
  * 2) RGB color generator function
  * 3) Collect all necessary referances 
- * 4) Handle the click event
+ * 4) RGB to HEX converter function
+ * 5) Handle the click event 
+ * 6) Show the result
 **/
 
 // Step 1: Create Oneload handeler
@@ -22,12 +24,13 @@ function main(){
     let changeBtn = document.getElementById('change-bg')
 
     changeBg(main, changeBtn)
+
 }
 
 
 // Step 2: RGB color generator function
 
-function generateColor(){
+function generateHEXColor(){
 
     let red, green, blue
 
@@ -37,23 +40,47 @@ function generateColor(){
 
     blue = Math.floor(Math.random() * 255 + 1)
 
-    return `rgb(${red}, ${green}, ${blue})`
+    return '#' + red.toString(16) + green.toString(16) + blue.toString(16)
 
 }
 
+// RGB to HEX converter function
+
+function hextoRgb(hex){
+    hex = hex.slice(1,6)
+    let red, green, blue
+    red = '0x' + hex.charAt(0) + hex.charAt(1)
+    green = '0x' + hex.charAt(2) + hex.charAt(3)
+    blue = '0x' + hex.charAt(4) + hex.charAt(5)
+
+    return `rgb(${red.toString(10)}, ${green.toString(10)}, ${blue.toString(10)})`
+}
+
 function changeBg(container, selector){
+
+    let hexColor = document.querySelector('.hex-code')
+    let rgbColor = document.querySelector('.rgb-code')
     
     // Change background by clicking button
 
     selector.addEventListener('click', (e) => {
-        container.style.background = generateColor()
+        container.style.background = generateHEXColor()
+        // printResult(hexColor, rgbColor)
     })
 
     // Change background by entering space button
     window.addEventListener('keypress', (e) => {
         if(e.key === ' '){
-            container.style.background = generateColor()
+            container.style.background = generateHEXColor()
         }
+        // printResult(hexColor, rgbColor)
     })
 
+}
+
+function printResult(hex, rgb){
+    let hexColor = generateHEXColor()
+    hex.innHTML = `HEX CODE: ${hexColor}`
+    let rgbColor = rgbToHex(hexColor)
+    rgb.innerHTML = `HEX CODE: ${rgbColor}`
 }
